441cf6e837c70d3b0c2d222107835ae8bb02dd91
Urban Kovač <urban.kovac@kalmia.si>
2024-02-16 08:57:58 +0100

    FE (import type fixes)

--- frontend/components/parts/Statistics.vue
+++ frontend/components/parts/Statistics.vue
@@ -21,7 +21,7 @@
 </template>
 
 <script lang="ts" setup>
-import { PropType } from 'vue';
+import { type PropType } from 'vue';
 
 defineProps({
   statistics: { type: Object as PropType<StatisticsInterface>, default: null },
