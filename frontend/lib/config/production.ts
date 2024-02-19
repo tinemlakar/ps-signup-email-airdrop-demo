3bee47da91acf1965a79f901a1e0af80c0b24045
Tadej Vengust <vengust.tadej@gmail.com>
2024-02-19 15:26:59 +0100

    set moonbeam chainid

--- frontend/lib/config/production.ts
+++ frontend/lib/config/production.ts
@@ -2,7 +2,7 @@ const config: ConfigInterface = {
   APP_URL: 'https://nft.ment.si',
   API_BASE: 'https://ment-api.apillon.io',
   CAPTCHA_KEY: 'f363ce6d-7543-4284-9caa-cf3219723f04',
-  CHAIN_ID: 1287,
+  CHAIN_ID: 1284,
   CONTRACT_ADDRESS: '0x208C1bfa90faC3aED3587a1D9d6F8119aF45FdA6',
   METADATA_BASE_URI:
     'https://k2k4r8p41ygxp9ogebtgstf7ix8qqn11qdcu2ld4u08896kei1wmcbj3.ipns.nectarnode.io/',
