ed9a4094e028a9f83d6e04ff8d593143ebd6af24
Urban Kovač <urban.kovac@kalmia.si>
2024-02-20 11:08:27 +0100

    MetaMask install link (link with path)

--- frontend/components/parts/form/Wallet.vue
+++ frontend/components/parts/form/Wallet.vue
@@ -2,6 +2,7 @@
 import WalletSVG from '~/assets/images/wallet.svg';
 import { useConnect } from 'use-wagmi';
 
+const { fullPath } = useRoute();
 const { connect, connectors, pendingConnector } = useConnect();
 </script>
 
@@ -23,7 +24,7 @@ const { connect, connectors, pendingConnector } = useConnect();
           v-if="!connector.ready && connector.id === 'metaMask'"
           type="secondary"
           size="large"
-          href="https://metamask.app.link/dapp/nft.ment.si"
+          :href="`https://metamask.app.link/dapp/nft.ment.si/${fullPath}`"
         >
           <span class="inline-flex gap-2 items-center">
             <NuxtIcon :name="connector.id" class="text-xl" filled />
