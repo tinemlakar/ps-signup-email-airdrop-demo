d16721d70b89f2868b56d7c585fc8dc72cd93e31
ZanJereb <zan_jereb@hotmail.com>
2024-02-22 10:25:08 +0100

    Changed colos on table components

--- frontend/components/parts/Table/Ellipsis.vue
+++ frontend/components/parts/Table/Ellipsis.vue
@@ -1,8 +1,8 @@
 <template>
   <div v-if="text" class="flex">
-    <n-ellipsis class="align-bottom" :line-clamp="1">{{ text }}</n-ellipsis>
+    <n-ellipsis class="text-body align-bottom" :line-clamp="1">{{ text }}</n-ellipsis>
     <button class="ml-2" @click="copyToClipboard(text)">
-      <span class="icon-copy"></span>
+      <span class="icon-copy text-body"></span>
     </button>
   </div>
 </template>
