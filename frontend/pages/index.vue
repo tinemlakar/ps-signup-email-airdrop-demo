a5d42a9688cdb6476b8f2d6b46a6f3ad425a0466
ZanJereb <zan_jereb@hotmail.com>
2024-02-22 08:21:40 +0100

    Reverted index page

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,28 +1,19 @@
 <script lang="ts" setup>
-import SuperRareJpg from '~/assets/images/superrare.jpg';
-
 useHead({
-  title: 'MENT NFT airdrop',
+  title: 'Apillon email airdrop prebuilt solution',
 });
+
+const registered = ref<boolean>(false);
 </script>
 
 <template>
-  <div class="my-10 flex flex-col gap-x-12 gap-y-8 md:flex-row lg:my-14">
-    <div class="max-w-2xl md:pr-12">
-      <span class="mb-2 inline-block text-xs font-bold uppercase tracking-[0.2em] text-info"
-        >The Shape of MENT to Come</span
-      >
-      <h1>Sign up and win your MENT token</h1>
+  <div v-if="registered" class="max-w-sm mx-auto">
+    <h3 class="my-8">You have successfully signed up for NFT Airdrop.</h3>
+    <p>You will receive an email with a claim link. Open the link and follow the instructions.</p>
+  </div>
+  <div v-else class="max-w-sm w-full mx-auto">
+    <h3 class="my-8">Sign up for airdrop</h3>
 
-      <p class="body-lg mb-6">
-        By signing up, you’ll be in the game for the hottest perks in the form of MENT token
-      </p>
-      <div class="max-w-sm">
-        <FormSighUp btn-text="Sign up" />
-      </div>
-    </div>
-    <div class="flex-cc relative w-full">
-      <img :src="SuperRareJpg" class="mx-auto" width="461" height="461" alt="Super rare NFT" />
-    </div>
+    <FormSighUp @submit-success="registered = true" />
   </div>
 </template>
